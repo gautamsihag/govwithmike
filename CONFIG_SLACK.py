6cd629aeafa200fa757b4cad52930d717440e1d4
gautam <gautamsihag297.com>
2016-02-24 20:31:05 +0000

    Adding the congig file

---
+++ CONFIG_SLACK.py
@@ -0,0 +1,8 @@
+# must create it by the user or use mine 
+# contains a single line,
+# url = 'users Slackbot's URL'. 
+# It should look something like 
+# https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX.
+
+
+url = 'https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX'
